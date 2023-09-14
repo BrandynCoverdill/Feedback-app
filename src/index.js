a5327ccb35b8d99cba980dedbcf46ec69d1d5466
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-09-14 07:23:20 -0600

    Finished lesson 2.6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react' // React Library
-import ReactDOM from 'react-dom/client' // ReactDOM Library
-import './index.css' // Imports global css file
-import App from './App' // Imports App component
+import React from 'react'; // React Library
+import ReactDOM from 'react-dom/client'; // ReactDOM Library
+import './index.css'; // Imports global css file
+import App from './App'; // Imports App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 ); // Passing in our main app component and displaying it in html.
