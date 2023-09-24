8a84d64f26b895016173212867ebe89aad5d8f4a
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-09-24 00:11:53 -0600

    Finished Lesson 3.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
 
-export default function Header({text}) {
+export default function Header({text, bgColor, textColor}) {
+  
+  const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+  }
+  
   return (
-    <header>
+    <header style={headerStyles}>
         <div className="container">
             <h2>{text}</h2>
         </div>
@@ -11,9 +17,13 @@ export default function Header({text}) {
 }
 
 Header.defaultProps = {
-    text: "Feedback UI"
+    text: "Feedback UI",
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
 }
 
 Header.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
 }
