a48aec707b75e2b3433f32e99f2e954b9863aa75
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-10-09 00:06:14 -0600

    Work from lesson 3.3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 export default function App() {
 	return (
-        <>
-            <Header 
-             />
-            <div className="container">
-                <h1>My App</h1>
-            </div>
-        </>
-    );
+		<>
+			<Header />
+			<div className='container'>
+				<FeedbackItem />
+			</div>
+		</>
+	);
 }
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default function FeedbackItem() {
+	const [rating, setRating] = useState(7);
+	const [text, setText] = useState('This is an example of a feedback item');
+
+	return (
+		<div className='card'>
+			<div className='num-display'>{rating}</div>
+			<div className='text-display'>{text}</div>
+		</div>
+	);
+}
