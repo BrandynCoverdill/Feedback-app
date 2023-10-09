42e0dcd90f8db8705ac9e92bf43dbc96d86c2f6e
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-10-09 03:30:09 -0600

    Work from Lesson 3.5

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,16 @@
+import PropTypes from 'prop-types';
+
+import Card from './shared/Card';
+
 export default function FeedbackItem({ feedback }) {
 	return (
-		<div className='card'>
+		<Card>
 			<div className='num-display'>{feedback.rating}</div>
 			<div className='text-display'>{feedback.text}</div>
-		</div>
+		</Card>
 	);
 }
+
+FeedbackItem.propTypes = {
+	item: PropTypes.object.isRequired,
+};
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
 
 export default function FeedbackList({ feedback }) {
 	if (!feedback || feedback.length === 0) {
@@ -13,3 +14,13 @@ export default function FeedbackList({ feedback }) {
 		);
 	}
 }
+
+FeedbackList.propTypes = {
+	feedback: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			rating: PropTypes.number.isRequired,
+			text: PropTypes.string.isRequired,
+		})
+	),
+};
