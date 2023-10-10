063f40a3e0b757e0b880f90461d762b8ef5b7953
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-10-10 03:20:11 -0600

    Fixed issue #1.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import { useState } from 'react';
+import {useState} from 'react';
 import feedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 
@@ -8,7 +8,7 @@ export default function App() {
 
 	function deleteFeedback(id) {
 		if (window.confirm('Are you sure you want to delete?')) {
-			setFeedback(feedbackData.filter((f) => id !== f.id));
+			setFeedback(feedback.filter((f) => id !== f.id));
 			console.log(`Deleted feedback id: ${id}`);
 		}
 	}
